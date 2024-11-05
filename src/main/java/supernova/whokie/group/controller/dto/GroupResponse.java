ba3f73dd9a545b8349b5f0fff141898ac0ad6b5d
momnpa333 <momnpa333@gmail.com>
2024-11-05 20:09:27 +0900

    Hotfix: group 생성시 group정보 반환

--- src/main/java/supernova/whokie/group/controller/dto/GroupResponse.java
+++ src/main/java/supernova/whokie/group/controller/dto/GroupResponse.java
@@ -6,15 +6,15 @@ import supernova.whokie.group.service.dto.GroupModel;
 public class GroupResponse {
 
     @Builder
-    public record Info(
+    public record InfoWithCount(
         Long groupId,
         String groupName,
         String groupImageUrl,
         Long groupMemberCount
     ) {
 
-        public static Info from(GroupModel.InfoWithMemberCount groupModel) {
-            return Info.builder()
+        public static InfoWithCount from(GroupModel.InfoWithMemberCount groupModel) {
+            return InfoWithCount.builder()
                 .groupId(groupModel.groupId())
                 .groupName(groupModel.groupName())
                 .groupImageUrl(groupModel.groupImageUrl())
@@ -31,6 +31,24 @@ public class GroupResponse {
         public static InviteCode from(GroupModel.InviteCode model) {
             return InviteCode.builder()
                 .inviteCode(model.inviteCode())
+                .build();
+        }
+    }
+
+    @Builder
+    public record Info(
+        Long groupId,
+        String groupName,
+        String groupDescription,
+        String groupImageUrl
+    ) {
+
+        public static Info from(GroupModel.Info model) {
+            return Info.builder()
+                .groupId(model.groupId())
+                .groupName(model.groupName())
+                .groupDescription(model.groupDescription())
+                .groupImageUrl(model.groupImageUrl())
                 .build();
         }
     }
