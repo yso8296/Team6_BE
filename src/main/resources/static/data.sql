56c407ae89999eccd2829480a73b1cd5ab9cf0a3
momnpa333 <momnpa333@gmail.com>
2024-11-05 16:03:33 +0900

    Feat: properties 수정

--- src/main/resources/static/data.sql
+++ src/main/resources/static/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO "groups" (created_at, modified_at, group_name, description, group_image_url)
-VALUES (NOW(), NOW(), 'COMMON', 'commonquestion.', 'group/default.png');
+INSERT INTO "groups" (group_name, description, group_image_url)
+VALUES ('COMMON', 'commonquestion.', 'group/default.png');
