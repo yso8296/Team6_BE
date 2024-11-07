d4da67a6897735117c385bda50a195b5580f12fb
momnpa333 <momnpa333@gmail.com>
2024-11-08 03:08:25 +0900

    Hotfix: 그룹 랭킹 조회 memberId로 반환

--- src/main/java/supernova/whokie/ranking/service/RankingReaderService.java
+++ src/main/java/supernova/whokie/ranking/service/RankingReaderService.java
@@ -1,5 +1,8 @@
 package supernova.whokie.ranking.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,13 +10,10 @@ import supernova.whokie.ranking.Ranking;
 import supernova.whokie.ranking.infrastructure.repoistory.RankingRepository;
 import supernova.whokie.ranking.service.dto.RankingModel;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class RankingReaderService {
+
     private final RankingRepository rankingRepository;
 
     @Transactional(readOnly = true)
@@ -24,12 +24,12 @@ public class RankingReaderService {
     @Transactional(readOnly = true)
     public RankingModel.Top3RankingEntries getTop3UsersFromGroupByGroupId(Long groupId) {
         List<Ranking> rankings = rankingRepository.findAllByGroupIdFetchJoinUsers(groupId);
-        Map<String, Integer> map = new HashMap<>();
+        Map<Long, Integer> map = new HashMap<>();
         for (Ranking ranking : rankings) {
-            if (!map.containsKey(ranking.getUsers().getName())) {
-                map.put(ranking.getUsers().getName(), 0);
+            if (!map.containsKey(ranking.getUsers().getId())) {
+                map.put(ranking.getUsers().getId(), 0);
             }
-            map.compute(ranking.getUsers().getName(), (k, value) -> value + ranking.getCount());
+            map.compute(ranking.getUsers().getId(), (k, value) -> value + ranking.getCount());
         }
 
         return RankingModel.Top3RankingEntries.from(map);
--- src/main/java/supernova/whokie/ranking/service/dto/RankingModel.java
+++ src/main/java/supernova/whokie/ranking/service/dto/RankingModel.java
@@ -1,60 +1,62 @@
 package supernova.whokie.ranking.service.dto;
 
-import lombok.Builder;
-import supernova.whokie.ranking.Ranking;
-
 import java.util.List;
 import java.util.Map;
+import lombok.Builder;
+import supernova.whokie.ranking.Ranking;
 
 public class RankingModel {
 
     @Builder
     public record Rank(
-            Long rankingId,
-            String question,
-            int rank,
-            int count,
-            String groupName
+        Long rankingId,
+        String question,
+        int rank,
+        int count,
+        String groupName
     ) {
+
         public static RankingModel.Rank from(Ranking entity, int rank) {
             return RankingModel.Rank.builder()
-                    .rankingId(entity.getId())
-                    .question(entity.getQuestion())
-                    .rank(rank)
-                    .count(entity.getCount())
-                    .groupName(entity.getGroups().getGroupName())
-                    .build();
+                .rankingId(entity.getId())
+                .question(entity.getQuestion())
+                .rank(rank)
+                .count(entity.getCount())
+                .groupName(entity.getGroups().getGroupName())
+                .build();
         }
     }
 
     @Builder
     public record GroupRank(
-            int rank,
-            int count,
-            String memberName
+        int rank,
+        int count,
+        Long memberId
     ) {
-        public static RankingModel.GroupRank from(Map.Entry<String, Integer> entry, int rank) {
+
+        public static RankingModel.GroupRank from(Map.Entry<Long, Integer> entry, int rank) {
             return RankingModel.GroupRank.builder()
-                    .rank(rank)
-                    .count(entry.getValue())
-                    .memberName(entry.getKey())
-                    .build();
+                .rank(rank)
+                .count(entry.getValue())
+                .memberId(entry.getKey())
+                .build();
         }
     }
 
     @Builder
     public record Top3RankingEntries(
-            List<Map.Entry<String, Integer>> entries
+        List<Map.Entry<Long, Integer>> entries
     ) {
-        public static Top3RankingEntries from(Map<String, Integer> map) {
-            List<Map.Entry<String, Integer>> entries = map.entrySet().stream()
-                    .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                    .limit(3)
-                    .toList();
+
+        public static Top3RankingEntries from(Map<Long, Integer> map) {
+            List<Map.Entry<Long, Integer>> entries = map.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .limit(3)
+                .toList();
 
             return Top3RankingEntries.builder()
-                    .entries(entries)
-                    .build();
+                .entries(entries)
+                .build();
         }
     }
 }
