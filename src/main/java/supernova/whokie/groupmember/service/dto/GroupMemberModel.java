9f564bbc16f8360627b9612cae90b730bd34a8cb
momnpa333 <momnpa333@gmail.com>
2024-11-08 20:04:09 +0900

    Hotfix: groupMemberRole 반환 api 생성

--- src/main/java/supernova/whokie/groupmember/service/dto/GroupMemberModel.java
+++ src/main/java/supernova/whokie/groupmember/service/dto/GroupMemberModel.java
@@ -1,63 +1,74 @@
 package supernova.whokie.groupmember.service.dto;
 
+import java.time.LocalDate;
+import java.util.List;
 import lombok.Builder;
 import supernova.whokie.groupmember.GroupMember;
 import supernova.whokie.groupmember.GroupRole;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public class GroupMemberModel {
 
     @Builder
     public record Member(
-            Long groupMemberId,
-            Long userId,
-            String userName,
-            String memberImageUrl,
-            LocalDate joinedAt,
-            GroupRole role
+        Long groupMemberId,
+        Long userId,
+        String userName,
+        String memberImageUrl,
+        LocalDate joinedAt,
+        GroupRole role
     ) {
 
         public static Member from(GroupMember groupMember, String imageUrl) {
             return Member.builder()
-                    .groupMemberId(groupMember.getId())
-                    .userId(groupMember.getUser().getId())
-                    .userName(groupMember.getUser().getName())
-                    .memberImageUrl(imageUrl)
-                    .joinedAt(groupMember.getCreatedAt().toLocalDate())
-                    .role(groupMember.getGroupRole())
-                    .build();
+                .groupMemberId(groupMember.getId())
+                .userId(groupMember.getUser().getId())
+                .userName(groupMember.getUser().getName())
+                .memberImageUrl(imageUrl)
+                .joinedAt(groupMember.getCreatedAt().toLocalDate())
+                .role(groupMember.getGroupRole())
+                .build();
         }
     }
 
     @Builder
     public record Members(
-            List<Member> members
+        List<Member> members
     ) {
 
         public static Members from(List<Member> members) {
             return Members.builder()
-                    .members(members)
-                    .build();
+                .members(members)
+                .build();
         }
     }
 
     @Builder
     public record Option(
-            Long groupMemberId,
-            Long userId,
-            String userName,
-            String imageUrl
+        Long groupMemberId,
+        Long userId,
+        String userName,
+        String imageUrl
     ) {
 
         public static Option from(GroupMember groupMember, String imageUrl) {
             return Option.builder()
-                    .groupMemberId(groupMember.getId())
-                    .userId(groupMember.getUser().getId())
-                    .userName(groupMember.getUser().getName())
-                    .imageUrl(imageUrl)
-                    .build();
+                .groupMemberId(groupMember.getId())
+                .userId(groupMember.getUser().getId())
+                .userName(groupMember.getUser().getName())
+                .imageUrl(imageUrl)
+                .build();
+        }
+    }
+
+    @Builder
+    public record Role(
+        GroupRole role
+    ) {
+
+        public static Role from(GroupRole role) {
+            return Role.builder()
+                .role(role)
+                .build();
         }
     }
 }
